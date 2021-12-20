f95a894bbbe2d1d8d23906024dd5c18925f20f98
KikiLP07 <65845464+KikiTheCoder@users.noreply.github.com>
2021-12-20 12:58:10 +0100

    Replaced hello-angular-gsap with niriteam-angular

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hello-angular-gsap'),
+      dir: require('path').join(__dirname, './coverage/niriteam-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
